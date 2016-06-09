e9a9608e73f35104a6ef43f597eb7318be4a472a
timo-stoettner <timo.stoettner@gmail.com>
2016-06-09 13:32:28 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ENN',
+  packages = ['ENN'],
+  version = '0.1',
+  description = 'Python Implementation of the Extended Nearest Neighbor Algorithm (ENN)',
+  author = 'Timo Stoettner',
+  author_email = 'mail@timo-stoettner.de',
+  url = 'https://github.com/timo-stoettner/ENN',
+  download_url = 'https://github.com/timo-stoettner/ENN/tarball/0.1',
+  keywords = ['ENN', 'Extended Nearest Neighbor', 'Machine Learning', 'Data Mining', 'KNN'],
+  classifiers = [],
+)
